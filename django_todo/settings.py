cdbdc82828ce13f5dd7118e66cbf523b2ef195dd
Chris <christoal@outlook.com>
2017-07-28 17:55:24 +0800

    added dj-database

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -92,13 +92,6 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- settings/staging.py
+++ settings/staging.py
@@ -1,5 +1,10 @@
 from base import *
+import dj_database_url
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['todo-api-institute.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
+}
