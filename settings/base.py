0ee2a0a2628d04fdcdebaa53f00dc60b2368866d
Chris <christoal@outlook.com>
2017-07-30 12:08:00 +0800

    settings update

--- settings/base.py
+++ settings/base.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corseheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
