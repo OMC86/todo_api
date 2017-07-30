29896cc4ae7d116b539a8bf4989a66a2791e73b6
Chris <christoal@outlook.com>
2017-07-30 11:09:43 +0800

    settings update

--- settings/staging.py
+++ settings/staging.py
@@ -5,12 +5,5 @@ DEBUG = False
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-DATABASES = {
     'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
